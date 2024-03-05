2c1b4eaf5d8e131c1a51159c29b4799738ff8a93
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2024-03-05 11:07:10 +0100

    Fix order of settings menu

--- src/main/js/index.ts
+++ src/main/js/index.ts
@@ -24,4 +24,4 @@
 import { ConfigurationBinder as cfgBinder } from "@scm-manager/ui-components";
 import NotifyConfigurationContainer from "./NotifyConfigurationContainer";
 
-cfgBinder.bindRepositorySetting("/notify", "scm-notify-plugin.navLink", "notifyConfig", NotifyConfigurationContainer);
+cfgBinder.bindRepositorySetting("/notify", "scm-notify-plugin.navLink", "notifyConfig", NotifyConfigurationContainer, "emailNotification");
